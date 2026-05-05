aa61472ac78c18012761d1c6d10b946ffffe47b8
Yash2121m <yashspatil2005@gmail.com>
2026-05-05 16:18:55 +0530

    Model Creation

---
+++ lib/models/category_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  final String name;
+  final IconData icon;
+  final double amount;
+
+  Category({required this.name, required this.icon, required this.amount});
+}
